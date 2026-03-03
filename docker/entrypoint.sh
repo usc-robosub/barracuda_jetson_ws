9665e59fbb458c30b6c826f9825c4a230b7331bb
Alejandro Arteaga <ajarteag@usc.edu>
2026-03-02 17:06:40 -0800

    fixed dockerfiles to build ros2 workspace while building image instead of starting up container and updated submodules

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,15 +2,6 @@
 set -e
 
 source /opt/ros/humble/setup.bash
-
-echo "Installing dependencies..."
-apt-get update
-
-rosdep install --from-paths src --ignore-src -y --skip-keys="python3-jetson-gpio"
-
-echo "Building ROS 2 workspace..."
-colcon build --symlink-install
-
 source install/setup.bash
 
 echo "=========================================="
